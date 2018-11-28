29536566971a5ee314ba6c2f55642c5df87bd9f3
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2018-11-27 22:41:33 -0200

    Added config to build and publish lib in hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ReactivehubSdkElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
     ]
   end
@@ -21,18 +23,22 @@ defmodule ReactivehubSdkElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:httpoison, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
-  
+
+  defp description() do
+    "reactivehub.io Elixir SDK"
+  end
+
   defp package() do
-    name: "reactivehub_sdk",
-    # These are the default files included in the package
-    files: ~w(lib .formatter.exs mix.exs README* readme* LICENSE*
-              license* CHANGELOG* changelog* src),
-    licenses: ["Apache 2.0"],
-    links: %{"GitHub" => "https://github.com/luizcaciatori/reactivehub-sdk-elixir"}
+    [
+      name: "reactivehub_sdk",
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/reactivehub-io/reactivehub-sdk-elixir"}
+    ]
   end
 end
